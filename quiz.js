5e36744fb9c910a7d18b88d454fae47ecf3ff8e5
amulya-parab <amulyaparab101@gmail.com>
2022-09-12 18:23:29 +0530

    Created JS file of quiz page

---
+++ quiz.js
@@ -0,0 +1,26 @@
+var quizForm = document.querySelector(".quiz-container");
+var submitAnswerBtn = document.querySelector("#submit-answers");
+var outputEl = document.querySelector("#output");
+
+var correctAnswers = [
+  "right-angled",
+  "obtuse",
+  "acute",
+  "60°",
+  "Equilateral triangle",
+];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  var formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+    outputEl.innerText = "Your score is " + score;
+  }
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
